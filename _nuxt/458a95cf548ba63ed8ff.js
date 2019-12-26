04608c69c37a00dd07c00c02870b1a5ab476f59b
Alejandro Parada <alejandroparada@hospitalevangelico.com>
2019-12-26 16:40:28 -0300

    Updates

---
+++ _nuxt/458a95cf548ba63ed8ff.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{314:function(n,t,e){"use strict";e.r(t);var c={},o=e(33),component=Object(o.a)(c,(function(){var n=this.$createElement;return(this._self._c||n)("div",{},[this._v("\n  About \n")])}),[],!1,null,"0d88c315",null);t.default=component.exports}}]);
